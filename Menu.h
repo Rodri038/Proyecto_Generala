ff650f810adc7feed11e9094210d30c3614882bd
Rodrigo <rburns038@gmail.com>
2022-10-30 15:12:24 -0300

    nuevos cambios en el menu, falta relacionar formulas de nacho y santi

---
+++ Menu.h
@@ -0,0 +1,49 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+void menu(){
+        int letra=0, rondas;
+        int puntaje=0, vDadosAzar[5];
+        char jugador1[20]; //jugador2[20];
+        int cursor=0;
+        const int inicial=49, comienzoDeTexto=50;
+        bool exit=true;
+
+        //hide cursor ocultar el cursor
+        rlutil::hidecursor();
+            do{
+                switch(cursor){
+                case 0:
+                    letra=74;
+                    break;
+                case 1:
+                    letra=74;
+                    break;
+                case 2:
+                    letra=80;
+                    break;
+                case 3:
+                    letra=83;
+                    break;
+                }
+        mostrarTexto("Elija una opcion para comenzar a jugar", 40,10);
+        mostrarTexto("j", inicial,11);
+        mostrarTexto("ugar de a UNO", comienzoDeTexto,11);//case 0
+        mostrarTexto("j", inicial,12);
+        mostrarTexto("ugar de a DOS", comienzoDeTexto,12);//case 1
+        mostrarTexto("p", inicial,13);
+        mostrarTexto("untaje   ", comienzoDeTexto,13);//case 2
+        mostrarTexto("s", inicial,14);
+        mostrarTexto("alir del juego", comienzoDeTexto,14);//case 3
+                //cursor
+                rlutil::locate(49, 11 + cursor);
+                cout<< (char)letra <<endl;
+
+//get key escucha la tecla que tocamos y devuelve un entero
+        int key = rlutil::getkey();
+        posicionDeCursor(inicial, 11, cursor, key);
+        accionesCursor(cursor, key, exit);
+    }while(exit);
+}
+
+#endif // MENU_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -3,54 +3,23 @@
 #include <iostream>
 #include <cstdlib>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "rlutil.h"
 
 using namespace std;
 
 #include "jugadorUno.h"
 #include "funciones.h"
+#include "Menu.h"
 
 int main(){;
 
 menu();
 
-return 0;
 }
 
-void menu(){
-        bool exit=true;
-        int opcion, puntaje=0, vDadosAzar[5], rondas;
-        char jugador1[20], jugador2[20];
-       do{
-                cout<<"---Elija una opcion---"<<endl;
-                cout<<"---Jugar de a UNO---[1]"<<endl;
-                cout<<"---Jugar de a DOS---[2]"<<endl;
-                cout<<"---Puntaje---[3]"<<endl;
-                cout<<"---Salir del juego---[4]"<<endl;
-                cin>>opcion;
 
-                switch(opcion){
-                case 1:
-                        pedirNombres(jugador1);
-                        cout<<"Cantidad de rondas???"<<endl;
-                        cin>>rondas;
-                        /*while(contaRondas<rondas){
-                            comienzo(jugador1, vDadosAzar, 5);
-                        }*/
-                    break;
-                case 2 :
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    exit=false;
-                    break;
-                default:
-                    cout<<"Ingrese una opcion CORRECTA"<<endl;
-                    cin>>opcion;
-                break;
-                }
- }while(exit);
-system("cls");
-}
 
 
